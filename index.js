158ba95c1e6c9e09e026bab2afe4d735db5323c6
Janusz Szubert <theinvalidnonce@gmail.com>
2018-01-30 20:18:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,4 +9,20 @@ class BoardMember {
     return 'No, I must disagree'
   }
   
+  approve() {
+    return 'You can do that!'
+  }
+  
+  doCharity() {
+    return 'I like to help people.'
+  }
+  
+  releasePressStatement() {
+    return 'You will see great things drom Scuber.'
+  }
+  
+  sayHi() {
+    return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}`
+  }
+  
 }
